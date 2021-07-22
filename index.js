e5881d2ed0973268c9353facdbcd1e1bb5648166
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-07-22 14:24:08 -0700

    task 1 complete, working on the Car class

--- index.js
+++ index.js
@@ -42,7 +42,25 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(someFood){
+    if(this.stomach.length < 10){
+      this.stomach.push(someFood);
+    }
+  }
+
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name} and ${this.age}.`
+  }
 }
 
 /*
@@ -60,7 +78,25 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.fuel = 0;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  fill(gallons){
+    this.tank += gallons;
+  }
+
+  drive(distance){
+    this.odometer += distance;
+    this.tank -= (distance / this.milesPerGallon);
+    
+    
+  }
+
 }
 
 /*
